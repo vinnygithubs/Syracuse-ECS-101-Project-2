4f2f60f787930f952d41a3e673538ab66aa824d1
vinnygithubs <vegallag@syr.edu>
2024-10-16 18:39:21 -0400

    Update same_function.py

--- same_function.py
+++ same_function.py
@@ -1,3 +1,14 @@
-def same(s1: str, s2: str) -> bool:
-    if s1 == s2:
-        return True 
+file_1='eeee'
+file_2='eeZee'
+
+def same(f1,f2):
+    for char in file_1:
+        for char2 in file_2:
+            if char2 == char:
+                continue
+            else:
+                print(char2,'in "file 2" not equal to',char,'in "file 1"')
+                return False
+    return True
+
+print(same(file_1,file_2))
