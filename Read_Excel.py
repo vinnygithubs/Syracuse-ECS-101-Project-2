988fbebb51edec6794b45ccb448d23e01f588b9d
vinnygithubs <vegallag@syr.edu>
2024-10-28 14:50:04 -0400

    Update Read_Excel.py

--- Read_Excel.py
+++ Read_Excel.py
@@ -1,11 +1,21 @@
-# Global Variables
+# title
+from importlib.resources import contents
+from operator import truediv
 import pandas as pd
+#
 XL = pd.read_excel('P2M012_G5.xlsx', dtype=str)
-# Columns
 Char = list(XL['Char'])
 Bin = list(XL['Bin'])
-Dec = list(XL['Dec'])
-#
-ShortNum = 5
-LongNum = 7
-#
+# variables
+shortnum = 5
+longnum = 7
+
+for index,item in enumerate(Char):
+    if item == '<space>':
+        Char[index] = " "
+    elif item == '<apostraphe>':
+        Char[index] = "'"
+    elif item == '<newline>':
+        Char[index] = "\n"
+
+print(Char)
