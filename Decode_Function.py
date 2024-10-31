fc902f40c094398d52250c3fce5aa030c050c5c3
vinnygithubs <vegallag@syr.edu>
2024-10-31 09:31:26 -0400

    Update Decode_Function.py

--- Decode_Function.py
+++ Decode_Function.py
@@ -1,20 +1,5 @@
 # // ECS M012, Group 5 Decode File Function
-from importlib.resources import contents
-from operator import truediv
-import pandas as pd
-#
-XL = pd.read_excel('P2M012_G5.xlsx', dtype=str)
-Char = list(XL['Char'])
-Bin = list(XL['Bin'])
-#
-shortnum=5
-longnum=7
-# sample=10010010000000101011001100011110101011011001101111 < - IGNORE (TESTING)
-#
-with open('BinOutput.txt', 'r') as fileopen:
-    oldfile = fileopen.read()
-    print("Encoded:", oldfile)
-
+# Vincent Gallagher
 # Decode function will loop through the file provided
 # in binary and loop through every char by tracking
 # the current spot. Make sure row isnt empty, then
@@ -23,40 +8,42 @@ with open('BinOutput.txt', 'r') as fileopen:
 # short numbers. continue until currspot < len of the
 # file provided.
 
-def decode_Start(info:str):
+def decode(fn="BinOutput.txt"):
+
+    with open(fn, 'r') as f:
+        fn = f.read()
+
+    fn = fn.split(".")[1]
+
     # WHILE THE FILE ISNT DECODED // LOOP
-    currentspot=0 # where we're at in the oldfile
+    currentspot = 0 # where we're at in the oldfile
     newfile = '' # the newfile str
 
     # LOOP THROUGH ALL NUMBERS BELOW
-    while currentspot < len(info): # while the cursor/spot is less than the length possible (of info)
-        if info[currentspot] == '0': # SHORT NUM
-            char_in_binary = info[currentspot:currentspot + shortnum] # start at curr spot up to 5 to get bin num
-            row_index = XL[XL['Bin'] == char_in_binary].index # get the row
-
-            if not row_index.empty:  # make sure row isnt empty
-                row = row_index[0]  # get the index of row (where it is )
-                the_char = XL.loc[row, 'Char']
-
-                newfile += the_char # add char
-                currentspot += shortnum  # Increase currspot for next loop
-                continue
-
-        elif info[currentspot] == '1': # LONG NUM
-            char_in_binary = info[currentspot:currentspot + longnum]
+    while currentspot < len(fn): # while the cursor < len
+        if fn[currentspot] == '0': # SHORT NUM
+            char_in_binary = fn[currentspot:currentspot + ShortNum]
+            # start at curr spot up to 5 to get bin num
             row_index = XL[XL['Bin'] == char_in_binary].index
-            # NO COMMENTS BECAUSE IT IS PRATICALLY THE SAME, BESIDE += longnum
-            if not row_index.empty:
-                row = row_index[0]
-                the_char = XL.loc[row, 'Char']
+            # get the row
+        elif fn[currentspot] == '1': # LONG NUM
+            char_in_binary = fn[currentspot:currentspot + LongNum]
+            # start at curr spot up to 7 to get bin num
+            row_index = XL[XL['Bin'] == char_in_binary].index
+            # get the row
 
-                newfile += the_char
-                currentspot += longnum
-                continue
+        else:
+            currentspot += 1
+            continue
 
-    return newfile
+        # deals with replaced values and makes sure they are good
+        if not row_index.empty:
+            row = row_index[0]
+            the_char = XL.loc[row, 'Char']
+            newfile += replacements.get(the_char, the_char)
+            currentspot += ShortNum if fn[currentspot] == '0' else LongNum
 
-new_content = decode_Start(oldfile)
-with open('TextOutput.txt', 'w') as fileopen:
-    fileopen.write(new_content)
-print("Decoded:", new_content)
+    TextOutput = open("TextOutput.txt")
+    # creates file 
+    with open('TextOutput.txt', 'w') as fileopen:
+        fileopen.write(newfile)
